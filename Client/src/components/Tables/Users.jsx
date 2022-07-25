bc25dd8dc7d3793882a05888f4036872f03ce5d4
Maraoui21 <y.maraoui@etu.enset-media.ac.ma>
2022-07-25 14:05:36 +0100

    user interface

--- Client/src/components/Tables/Users.jsx
+++ Client/src/components/Tables/Users.jsx
@@ -4,7 +4,7 @@ import { headers } from "../Accueil";
 
 const Users = ( )=>{
     const [users,setUsers]=useState([]);
-	const [updateUserModal, setUpdateShowModal] = useState(false);
+	const [updateUserModal, setUpdateModal] = useState(false);
     const [updateMessage,setUpdateMessage] = useState();
 
 	const  tableContainer = {
@@ -83,7 +83,7 @@ const Users = ( )=>{
 											<div class="flex item-center justify-center">
 												<div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
 													<div>
-														<div  onClick={() => setUpdateShowModal(true)}>
+														<div  onClick={() => setUpdateModal(true)}>
 														<svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 															<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
 														</svg>
@@ -116,7 +116,7 @@ const Users = ( )=>{
             {/*content*/}
             <div className="p-10 border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 				<div>
-					<button onClick={() => setUpdateShowModal(false)} className="text-gray-400 float-right">
+					<button onClick={() => setUpdateModal(false)} className="text-gray-400 float-right">
 						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
 						</svg>
